6c5b56b2920f0dde87a497164b287b1b57006674
vince2k6 <training@training-instance-6.c.sandbox-wescale.internal>
2017-06-27 12:42:20 +0000

    fix

--- src/main/java/projectx_web/X.java
+++ src/main/java/projectx_web/X.java
@@ -1,15 +1,14 @@
 package projectx_web;
 
-import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Application;
 import javax.ws.rs.core.MediaType;
+
 import org.json.JSONObject;
 
-@ApplicationPath("/projectx")
-public class X extends Application {
+@Path("/x")
+public class X {
 
     /**
      * Retrieves representation of an instance of Test
@@ -17,7 +16,6 @@ public class X extends Application {
      * @return an instance of String
      */
     @GET
-    @Path("/x")
     @Produces(MediaType.APPLICATION_JSON)
     public String resourceMethodGET() {
         JSONObject jsonObject = new JSONObject();
@@ -25,4 +23,9 @@ public class X extends Application {
         return jsonObject.toString();
     }
 
+    public static void main(String args[]) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("result", "OK");
+        System.out.println(jsonObject);
+    }
 }
